8f439a8c91b541be4782a5caf94621af24ef9056
Takashi Kato <tohosaku@users.osdn.me>
2023-12-11 08:06:10 +0900

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_simple_example do
+  name 'Redmine Simple Example plugin'
+  author 'John doe'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://github.com/tohosaku/redmine_simple_example'
+  author_url 'http://github.com/tohosaku'
+end
---
+++ lib/redmine_simple_example/sample.rb
@@ -0,0 +1,7 @@
+module RedmineSimpleExample
+  class Sample
+    def self.hello
+      "hello"
+    end
+  end
+end
